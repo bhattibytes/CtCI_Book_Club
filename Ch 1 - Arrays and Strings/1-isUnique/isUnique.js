31af2dec500de58bc3d018638a49e694375e4ac2
Jason Bhatti <bhattibytes@gmail.com>
2023-07-31 15:47:22 -0400

    Implement - isUnique

--- Ch 1 - Arrays and Strings/1-isUnique/isUnique.js
+++ Ch 1 - Arrays and Strings/1-isUnique/isUnique.js
@@ -1,7 +1,13 @@
 //Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
 
 function isUnique(str) {
-  //code
+  stringArr = str.split('');
+  stringSet =  new Set(str);
+
+  if (stringArr.length !== stringSet.length) {
+    return false;
+  }
+  return true;
 }
 
 module.exports = isUnique;
